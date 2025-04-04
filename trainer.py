adadc168fa4715de2eeeb5c52b60896b5323bcd5
YuhanXu718 <xyhcl940718@gmail.com>
2025-04-04 19:13:13 +0200

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1,7 +1,4 @@
-"""
-Copyright (C) 2017 NVIDIA Corporation.  All rights reserved.
-Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
-"""
+
 from networks import AdaINGen, MsImageDis, VAEGen
 from utils import weights_init, get_model_list, vgg_preprocess, load_vgg16, get_scheduler
 from torch.autograd import Variable
