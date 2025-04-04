a4b77119bcf7added4d43b1b6a4433625c1c186d
YuhanXu718 <xyhcl940718@gmail.com>
2025-04-04 19:13:37 +0200

    Update test_batch.py

--- test_batch.py
+++ test_batch.py
@@ -1,7 +1,4 @@
-"""
-Copyright (C) 2018 NVIDIA Corporation.  All rights reserved.
-Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
-"""
+
 from __future__ import print_function
 from utils import get_config, get_data_loader_folder, pytorch03_to_pytorch04
 from trainer import MUNIT_Trainer, UNIT_Trainer
